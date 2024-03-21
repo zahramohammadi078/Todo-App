593593472247393b8a66cfbd32d9df806d158386
zahra <zaramoon99123@gmail.com>
2024-03-22 00:39:00 +0330

    change

--- scriot.js
+++ scriot.js
@@ -4,16 +4,70 @@ let card = document.querySelector(".card");
 let todoArray = [];
 
 addBtn.addEventListener("click", addBtnFunc);
-
+inputBox.addEventListener("keydown", enterFunc);
+/*--------------------------creat btn and items----------------------------------*/
 function creatTodoItem(todoObj){
-  let todoItem=document.createElement("div")
-  todoItem.classList.add("add-new-item")
-  todoItem.textContent=todoObj.title
 
-  card.appendChild(todoItem)
+
+  let newDivOne=document.createElement("div")
+  newDivOne.classList.add("todo-items")
+  let newP=document.createElement("p")
+  newP.classList.add("todotext")
+  newP.textContent=todoObj.title
+  // newDivOne.append(newP)
+  let newDivTwo=document.createElement("div")
+  newDivTwo.classList.add("allbtns")
+  //btnOne
+  let btnOne=document.createElement("button")
+  btnOne.classList.add("btn")
+  btnOne.classList.add("Complate")
+  btnOne.textContent="Complate"
+// newDivTwo.append(btnOne)
+//btnTwo
+  let btnTwo=document.createElement("button")
+  btnTwo.classList.add("btn")
+  btnTwo.classList.add("b1")
+  btnTwo.classList.add("Delete")
+  btnTwo.textContent="Delete"
+// newDivTwo.append(btnTwo)
+//btnThree
+  let btnThree=document.createElement("button")
+  btnThree.classList.add("btn")
+  btnThree.classList.add("b1")
+  btnThree.classList.add("Edite")
+  btnThree.textContent="Edite"
+newDivTwo.append(btnOne,btnTwo,btnThree)
+
+newDivOne.append(newP,newDivTwo)
+
+card.append(newDivOne)
+
+
+  
 
 
 }
+/*--------------------------enter and keydown-------------------------------*/
+function enterFunc(event){
+
+  if (event.keyCode === 13) {
+    if (inputBox.value) {
+      let todoValue = inputBox.value.trim();
+      let todoObj = {
+        id: todoArray.length + 1,
+        title: todoValue,
+        complete: false, // Change 'complete' to 'false' initially
+      };
+      todoArray.push(todoObj);
+      creatTodoItem(todoObj);
+      inputBox.value = "";
+    } else {
+      alert("It is empty");
+    }
+  }
+
+}
+/*------------------------click add btn--------------------------------*/
 
 function addBtnFunc() {
   // let todoValue=inputBox.value
@@ -23,7 +77,7 @@ function addBtnFunc() {
     let todoObj = {
       id: todoArray.length + 1,
       title: todoValue,
-      complate: false,
+      complate: true,
     };
     todoArray.push(todoObj);
     console.log(todoObj);
@@ -41,7 +95,7 @@ function addBtnFunc() {
   console.log(todoArray);
 }
 
-
+/*---------------------------------------------*/
 ////
 function makeTodo(){
 
@@ -50,3 +104,7 @@ function makeTodo(){
  
 
 }
+
+
+/////
+
